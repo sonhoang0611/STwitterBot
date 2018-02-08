445bcfdfd3042825407ff2f77d750b14da135a86
sonhoang0611 <hoangthaison985@gmail.com>
2018-02-08 20:42:48 +0700

    update information

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // make requests to Twitter's APIs
 var Twit = require('twit')
 var T = new Twit({
-    consumer_key:         'JxksDP3w99BKkNsYHGwOt8uxy',
-    consumer_secret:      '7ih8Rnr9wQduAxCRgYQAqFwYk28yLo4Jf7S1CNF70SkKwoG6Zw',
-    access_token:         '828564226629197824-abUl6WO9ILW3OYqREvBdnyD1diNjSnC',
-    access_token_secret:  'rcmzg6mXu0zdpMQjkmsYxmiKudQipxiSymFHOnhISN9a8',
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
 })
 
 //  id of users u want to retweet
